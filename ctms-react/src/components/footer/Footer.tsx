4288c8a41883943b4e864b55f9b63ee48cca4b40
cesa <alessio.cesaroni@live.com>
2026-07-27 22:25:00 +0200

    modifica posizione  immagine admin

--- ctms-react/src/components/footer/Footer.tsx
+++ ctms-react/src/components/footer/Footer.tsx
@@ -1,11 +1,11 @@
 import styles from "./Footer.module.css"
-import logoAziendale from '../../../assets/admin.png';
+import logoAziendale from '../../assets/admin.png';
 import { BACKEND_URL } from "../config";
 export default function Footer(){
     return(
         <div className={styles.footerContainer}>
             <a href={`${BACKEND_URL}/admin`}>
-            <img src={logoAziendale} alt="Logo Admin" className={styles.adminImmage} />
+                <img src={logoAziendale} alt="Logo Admin" className={styles.adminImmage} />
             </a>
         </div>
     )
